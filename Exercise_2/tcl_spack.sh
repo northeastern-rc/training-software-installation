a03f78abd85c25909f4446a0ba7056bb5f8f0adc
Prasanth-Dwadasi <prasanthsai448@gmail.com>
2023-06-10 12:47:41 -0400

    Correcting scripts

--- Exercise_2/tcl_spack.sh
+++ Exercise_2/tcl_spack.sh
@@ -15,15 +15,18 @@
 #####Use after training##############
 ##SBATCH --partition=express
 
+## Load python module rwquired for spack
+module load python/3.8.1
+
 ## Source spack environmnet:
 source ~/spack/share/spack/setup-env.sh
 
 ## Install a package (uncomment this line for a first-time install):
-# spack install tcl@8.6.12
+#spack install tcl@8.6.12
 
 ## Source Spack again, and load the package:
 source ~/spack/share/spack/setup-env.sh
 spack load tcl
 
-# Print the LD_LIBRARY_PATH
+# Print the PATH
 echo $PATH
